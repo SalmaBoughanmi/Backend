510a63f8b4b6b739e52a01ebab3a9fdf555c2d35
SalmaBoughanmi <129304685+SalmaBoughanmi@users.noreply.github.com>
2023-04-27 02:27:40 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetPFE/Entities/offre.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetPFE.Entities
+{
+    public class offre
+    {
+        public int offre_id { get; set; }
+        public int nb_poste { get; set; }
+        public string? fonction { get; set; }
+        public string? description { get; set; }
+        public string? mission { get; set; }
+        public ICollection<demande>? demandes { get; set; }
+    }
+}
